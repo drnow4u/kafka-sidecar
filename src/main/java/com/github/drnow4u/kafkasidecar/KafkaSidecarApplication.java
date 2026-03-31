2e811c693460735253c9cdb02faee5d0d2f410c5
Nowak M. (Marcin) <marcin.nowak3@ing.com>
2026-03-31 15:39:50 +0200

    Spring Boot

---
+++ src/main/java/com/github/drnow4u/kafkasidecar/KafkaSidecarApplication.java
@@ -0,0 +1,13 @@
+package com.github.drnow4u.kafkasidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSidecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSidecarApplication.class, args);
+    }
+
+}
