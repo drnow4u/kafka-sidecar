7d07a65edcd711e949219dc8b6dc699d9ab5da93
Nowak M. (Marcin) <marcin.nowak3@ing.com>
2026-04-01 09:11:10 +0200

    Spring Boot

---
+++ fake-consumer/src/main/java/com/github/drnow4u/fakeproducer/FakeProducerApplication.java
@@ -0,0 +1,13 @@
+package com.github.drnow4u.fakeproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeProducerApplication.class, args);
+    }
+
+}
