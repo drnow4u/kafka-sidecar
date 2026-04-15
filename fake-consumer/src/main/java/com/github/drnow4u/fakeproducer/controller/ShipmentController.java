78d4bfb8ff4d38b863fe329614fe92b25dd2d64a
Nowak M. (Marcin) <marcin.nowak3@ing.com>
2026-04-15 20:12:52 +0200

    Spring Boot

--- fake-consumer/src/main/java/com/github/drnow4u/fakeproducer/controller/ShipmentController.java
+++ fake-consumer/src/main/java/com/github/drnow4u/fakeproducer/controller/ShipmentController.java
@@ -4,18 +4,13 @@ import com.github.drnow4u.fakeproducer.model.Shipment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @RestController
